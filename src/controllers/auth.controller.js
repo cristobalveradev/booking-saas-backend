12fb8367f9929cf14fc73f504032dab8926a6024
Cristobal Vera <cristobalverar97@gmail.com>
2026-05-31 23:29:48 +0200

    add login endpoint with JWT authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const pool = require("../config/db");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
     try{
@@ -37,4 +38,42 @@ const register = async (req, res) => {
     }
 }
 
-module.exports = {register};
+const login = async (req,res) => {
+    try{
+        const {email, password} = req.body;
+        
+        // 1. check if user exists
+        const user = await pool.query(
+            "SELECT * FROM users WHERE email = $1",
+            [email]
+        );
+        
+        if (user.rows.length === 0){
+            return res.status(400).json({message:"Invalid Credentials"})
+        }
+        // 2. compare passwords
+        const validPassword = await bcrypt.compare(
+            password,
+            user.rows[0].password_hash
+        );
+
+        if(!validPassword){
+            res.status(400).json({message:"Invalid Credentials"})
+        }
+
+        // 3. Generate token
+
+        const token = jwt.sign(
+            {userId:user.rows[0].id},
+            process.env.JWT_SECRET,
+            {expiresIn:"1d"}
+        );
+
+        res.json({token})
+        
+    } catch(err){
+        console.error(err)
+        res.status(500).json({message:"Server error"})
+    }
+}
+module.exports = {register, login};
