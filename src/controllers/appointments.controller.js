47a9df8ebf65585f6c2a5d7cdd2d15ab2e5ac6d9
Cristobal Vera <cristobalverar97@gmail.com>
2026-06-09 21:17:52 +0200

    add deleteAppointment and getAppointmentById features Also added routes files to make them accesible

--- src/controllers/appointments.controller.js
+++ src/controllers/appointments.controller.js
@@ -1,32 +1,68 @@
 const pool = require("../config/db")
 
 const createNewAppointment = async (req,res) => {
-   const {serviceId, start_time, date, userId , duration} = req.body;
-   const splitTime = start_time.split(":")
-    
-   // logic to get appoinment end time
-   const x = new Date(`${date}T${start_time}:00`) 
-   x.setMinutes(x.getMinutes() + duration)
-   const end_time = x.toTimeString().slice(0,5)
-   
-   const overlappingAppointments = await pool.query("SELECT id FROM appointment WHERE user_id = $1 AND date = $2 AND $3 < end_time AND $4 > start_time", [userId, date, start_time, end_time ])
-   console.log(overlappingAppointments)
-    
-   if(overlappingAppointments.rowCount>0){
-    return res.status(409).send({messagge:"Service already booked in thhis time range"})     
-   }
-   
-   const newAppointmen = await pool.query("INSERT INTO appointment (user_id, service_id, date, start_time, end_time, customer_name, customer_email,created_at) VALUES($1,$2,$3,$4,$5,$6, $7, now() )", [userId, serviceId,date,start_time, end_time, "john doe", "john@doe.com"])
-   if (newAppointmen.rowCount == 1){
-    return res.status(200).send({message:"Appointmen created"})
-   }
-   //end_time::time + INTERVAL '70 minutes' as x
-    //const end_time = start_time + serviceDuration
+    try{
 
-    console.log(overlappingAppointments.rows.length)
-    return res.send({message:"Request received"})
-
+        const {serviceId, start_time, date, userId , duration} = req.body;
+        const splitTime = start_time.split(":")
+        
+    // logic to get appoinment end time
+    const x = new Date(`${date}T${start_time}:00`) 
+    x.setMinutes(x.getMinutes() + duration)
+    const end_time = x.toTimeString().slice(0,5)
+    
+    const overlappingAppointments = await pool.query("SELECT id FROM appointment WHERE user_id = $1 AND date = $2 AND $3 < end_time AND $4 > start_time", [userId, date, start_time, end_time ])
+        
+    if(overlappingAppointments.rowCount>0){
+        return res.status(409).send({messagge:"Service already booked in thhis time range"})     
+    }
+    
+    const newAppointmen = await pool.query("INSERT INTO appointment (user_id, service_id, date, start_time, end_time, customer_name, customer_email,created_at) VALUES($1,$2,$3,$4,$5,$6, $7, now() )", [userId, serviceId,date,start_time, end_time, "john doe", "john@doe.com"])
+    if (newAppointmen.rowCount == 1){
+        return res.status(200).send({message:"Appointmen created"})
+    }
+    
+    } catch(err){
+        return res.status(500).send({message:"Server connection failes"})
+    }   
+    
 
 }
 
-module.exports = {createNewAppointment}
+const deleteAppointment = async (req, res) => {
+    try{
+        const {appointmentId} = req.body;
+        const deletedAppointment = await pool.query("DELETE FROM appointment WHERE id = $1", [appointmentId])
+        
+        console.log(deletedAppointment.rowCount)
+        if (deletedAppointment.rowCount == 0){
+            return res.status(404).send({message:"Coudln't find the resourse"})
+        } 
+        
+        return res.status(200).send({message:"Appointment deleted"})
+    } catch(err){
+        console.log(err.code)
+        res.status(500).send({message:"Server error"})       
+    }
+}
+
+const getAppointmentsByUserId = async (req, res) => {
+    
+    try{
+        const {userId} = req.body;
+        const appointments = await pool.query("SELECT * FROM appointment WHERE user_id = $1", [userId])
+        if(appointments.rowCount > 0){
+            return res.status(200).send({
+                mesage:"Request succesfull",
+                data: appointments.rows
+            })
+        }
+        return res.status(404).send({message:"No appointmens found"})
+        
+    } catch(err){
+        return res.status(500).send({message:"Server error"})
+    }
+}
+
+
+module.exports = {createNewAppointment, deleteAppointment, getAppointmentsByUserId}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,13 +3,16 @@ const router = express.Router();
 const { authenticateToken } = require("../middlewares/auth.middleware");
 const { register, login } = require("../controllers/auth.controller");
 const { createService, getServices,  deleteService }  = require("../controllers/service.controller");
-const { createNewAppointment } = require("../controllers/appointments.controller");
+const { createNewAppointment, deleteAppointment, getAppointmentsByUserId } = require("../controllers/appointments.controller");
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/createService", authenticateToken, createService )
-router.get("/getServices", authenticateToken, getServices)
+router.post("/createService", authenticateToken, createService)
+router.get("/getServices/:id", authenticateToken, getServices)
 router.post("/createAppointment", createNewAppointment)
 router.delete("/deleteService", deleteService)
+router.delete("/deleteAppointment", deleteAppointment)
+router.get("/getApointmentsById", getAppointmentsByUserId)
+
 
 module.exports = router;
